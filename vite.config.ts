cd213651a40333459512c802e93a0a8bd1fcde1a
Erick Pac <erickj.pac@gmail.com>
2025-01-07 13:27:39 -0600

    update package.json and vite.config.ts to add vite-tsconfig-paths for improved TypeScript path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig, AliasOptions } from "vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-const root = path.resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": root,
-    } as AliasOptions,
-  },
+  plugins: [react(), tsconfigPaths()],
 });
