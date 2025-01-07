170fd3546d0201e3661f88d27a9a8b1d476adaa8
Erick Pac <erickj.pac@gmail.com>
2025-01-07 17:19:46 -0600

    add Vitest setup and tests for App component

---
+++ src/components/__tests__/app.test.tsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+import App from "../app";
+
+describe("App", () => {
+  it("renders App component", () => {
+    render(<App />);
+    expect(
+      screen.getByText("pages.home.title", { exact: false }),
+    ).toBeInTheDocument();
+  });
+
+  it("renders with correct class names", () => {
+    render(<App />);
+    const divElement = screen.getByRole("heading", { level: 1 });
+    expect(divElement).toHaveClass("text-4xl font-bold text-blue-500");
+  });
+});
