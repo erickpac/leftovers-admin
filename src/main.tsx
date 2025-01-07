a0b6d1de343003004ba851064da88fd887f8e0e1
Erick Pac <erickj.pac@gmail.com>
2025-01-07 17:43:38 -0600

    update i18n import path in main.tsx for configuration consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
-import "@/i18n/i18n";
+import "@/i18n/config";
 import App from "@/components/app";
 
 createRoot(document.getElementById("root")!).render(
