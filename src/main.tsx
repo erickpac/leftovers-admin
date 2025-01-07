e7622c42516e31e8ce6a990c7d110c7e60f478a4
Erick Pac <erickj.pac@gmail.com>
2025-01-07 13:17:38 -0600

    update i18n import path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
-import "./locales/i18n";
+import "./i18n/i18n";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
