875ee0af348e340aa0b75ba523e756f01bb0a297
Erick Pac <erickj.pac@gmail.com>
2025-01-07 13:24:57 -0600

    add TypeScript path alias and update ESLint configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import "./i18n/i18n";
-import App from "./App.tsx";
+import App from "@/components/app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
